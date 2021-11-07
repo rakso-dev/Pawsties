42ef1255a1460918395b769b8714dc82ff473d7a
Joel Jacome Pioquinto <87150153+joeljacp14@users.noreply.github.com>
2021-11-07 14:55:06 -0600

    Add files via upload

--- app/src/main/java/com/example/pawsties/MainActivity.java
+++ app/src/main/java/com/example/pawsties/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.pawsties;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationBar = findViewById(R.id.bottom_navigation_bar);
+        navigationBar.setOnNavigationItemSelectedListener(navigationListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new ProfilesFragment()).commit();
     }
+
+    private OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_profiles:
+                    selectFragment = new ProfilesFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectFragment).commit();
+            return true;
+        }
+    };
 }
--- app/src/main/java/com/example/pawsties/ProfilesFragment.java
+++ app/src/main/java/com/example/pawsties/ProfilesFragment.java
@@ -1,2 +1,19 @@
-package com.example.pawsties;public class Fragments {
+package com.example.pawsties;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProfilesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_profiles, container, false);
+    }
 }
