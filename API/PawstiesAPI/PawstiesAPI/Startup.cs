ca02a1c628d908d9261be4d894b1a0b348f53700
Rakso <oskarr.magana@gmail.com>
2021-12-04 14:06:55 -0600

    minor changes

--- API/PawstiesAPI/PawstiesAPI/Startup.cs
+++ API/PawstiesAPI/PawstiesAPI/Startup.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace PawstiesAPI
 {
@@ -27,6 +27,10 @@ namespace PawstiesAPI
         {
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PawstiesAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,9 +39,9 @@ namespace PawstiesAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PawstiesAPI v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
