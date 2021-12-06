285df83b63943cab57b728416bf214e60c5e4329
Rakso <oskarr.magana@gmail.com>
2021-12-06 17:07:53 -0600

    postGIS configured, working on controllers. Rescatista controller throws error on JSON conversion

---
+++ API/PawstiesAPI/PawstiesAPI/Controllers/MascotaController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PawstiesAPI.Models;
+
+namespace PawstiesAPI.Controllers
+{
+    public class MascotaController: ControllerBase
+    {
+        private readonly pawstiesContext _context;
+        private readonly ILogger<MascotaController> _logger;
+
+        public MascotaController(pawstiesContext context, ILogger<MascotaController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet ("pawstiesAPI/mascotas/all")]
+        [ProducesResponseType (StatusCodes.Status200OK, Type = typeof(IEnumerable<Mascotum>))]
+        [ProducesResponseType (StatusCodes.Status500InternalServerError)]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Calling method GetAllMascotas");
+            var mascota = _context.Mascota;
+            return Ok (mascota);
+        }
+
+        [HttpGet ("pawstiesAPI/mascotas/{petID}")]
+        [ProducesResponseType (StatusCodes.Status200OK, Type = typeof(Mascotum))]
+        [ProducesResponseType (StatusCodes.Status500InternalServerError)]
+        public IActionResult GetMascota(int petID)
+        {
+            _logger.LogInformation($"Calling method GetMascota with petID {petID}", null);
+            var mascota = _context.Mascota.Where(e => e.Petid == petID).FirstOrDefault();
+            return Ok(mascota);
+        }
+    }
+}
---
+++ API/PawstiesAPI/PawstiesAPI/Controllers/RescatistaController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using PawstiesAPI.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace PawstiesAPI.Controllers
+{
+    public class RescatistaController: ControllerBase
+    {
+        private readonly pawstiesContext _context;
+        private readonly ILogger<RescatistaController> _logger;
+
+        public RescatistaController(pawstiesContext context, ILogger<RescatistaController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet ("pawstiesAPI/rescatista/all")]
+        
+        public IActionResult Get()
+        {
+            var rescatista = _context.Rescatista;
+            return Ok(rescatista);
+        }
+
+        [HttpGet ("pawstiesAPI/rescatista/{rescatistaID}")]
+        [ProducesResponseType (StatusCodes.Status200OK, Type = typeof(Rescatistum))]
+        [ProducesResponseType (StatusCodes.Status500InternalServerError)]
+        public IActionResult GetRescatista(int rescatistaID)
+        {
+            Rescatistum rescatista = null;
+
+            try
+            {
+                rescatista = _context.Rescatista.Where(e => e.Rescatistaid == rescatistaID).FirstOrDefault();
+                return Ok(rescatista);
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during query to Rescatista table");
+                throw;
+            }
+        }
+    }
+
+    public interface DTO
+    {
+        public void Algo();
+    }
+
+    public class RescatistaDTO: DTO
+    {
+        public string Image { get; set; }
+        public string Mail { get; set; }
+        public string Telephone { get; set; }
+        public int Rescatistaid { get; set; }
+        public string NombreEnt { get; set; }
+        public string Rfc { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+
+        public void Algo() {
+            
+        }
+
+        
+    }
+}
