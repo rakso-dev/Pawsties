d7a3b078fff1fbb24ac72c87fb00ababd5bb47ac
joeljacp14 <joeljacomep@outlook.com>
2021-12-07 21:51:12 -0600

    findviews on Singup activity

--- androidApp/app/src/main/java/com/pawsties/android/SignupActivity.java
+++ androidApp/app/src/main/java/com/pawsties/android/SignupActivity.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Date;
+
 public class SignupActivity extends AppCompatActivity {
     EditText etEmail, etPassword;
+    RadioButton rbAdoptante, rbRescatista;
     Button registrarse;
-    String email="", password="";
+    String name="", lastname="",  email="", password="", typeUser="";
+    Date nacimiento;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,12 +28,19 @@ public class SignupActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Registro");
 
         /** LLAMAR A TODS LOS ELEMENTOS DEL LAYOUT ACTIVITY SIGN UP PARA OBTENER LOS DATOS*/
+        rbAdoptante = findViewById(R.id.rbAdoptante);
+        rbRescatista = findViewById(R.id.rbRescatista);
         registrarse = findViewById(R.id.btnOkSignup);
 
         registrarse.setOnClickListener(v -> {
             /**descomentar despues para hacer las validaciones*/
-//            email = etEmail.getText().toString();
-//            password = etPassword.getText().toString();
+            if (rbAdoptante.isActivated())//no lo hace
+                typeUser = "A";
+            if (rbRescatista.isActivated())
+                typeUser = "R";
+            Toast.makeText(SignupActivity.this, "El usuario es "+typeUser, Toast.LENGTH_SHORT).show();
+            email = etEmail.getText().toString();
+            password = etPassword.getText().toString();
 //
 //            if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password))
 //                Toast.makeText(SignupActivity.this, "Debes llenar todos los campos antes de continuar!!!", Toast.LENGTH_LONG).show();
